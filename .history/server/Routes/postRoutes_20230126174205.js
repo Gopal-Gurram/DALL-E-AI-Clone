841f4502ed7393fb9380b6e1916b57cadb8f30c3
Gopal Gurram <59303056+Gopal-Gurram@users.noreply.github.com>
2023-01-27 10:00:34 +0530

    Delete .history directory

--- .history/server/Routes/postRoutes_20230126174205.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import { Configuration, openAIApi } from "openai";
-
-import Post from "../mongodb/models/post.js";
-
-dotenv.config();
-const router = express.Router();
